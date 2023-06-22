e10dc13b2c666ef25e333fbcd456aef0169138a2
aelias-c <66627234+aelias-c@users.noreply.github.com>
2023-06-22 11:37:40 -0400

    Update BTIM.py

--- BTIM.py
+++ BTIM.py
@@ -1,40 +1,40 @@
-import sys
 import numpy as np
 from pandas import date_range
 
-from utils import len_month, monthly_out_name, month_names_aug
+from utils import len_month, monthly_out_name, month_names_aug, prepare_filenames, read_month, read_day, t2m_freq, tp_freq, standardize_precip
 from square_mask import square_mask
 from time_step import Brasnett
+from rescaling import mult_scaling
 from save_daily import save_daily
 from save_annual import save_annual
 
 import CONFIG as cfg
+
 # ------- Initialize -------- #
-year = int(sys.argv[1])
 
-snow_season = [year, year+1]
-year_tag = str(year)+'_'+str(year+1)
+snow_season = [cfg.year, cfg.year+1]
+t2m_files, tp_files = prepare_filenames(cfg.forcing, cfg.data_loc, snow_season)
 
-t2m_files, tp_files = cfg.prepare_filenames(snow_season)
-
+year_tag = str(snow_season[0])+'_'+str(snow_season[1])
 print('Processing snow year: ' + year_tag)
 
 # --- Step month by month --- #  
 for i,m in enumerate([8,9,10,11,12,1,2,3,4,5,6,7]):
     
-    current_y = year
+    current_y = snow_season[0]
     if m < 8:
-        current_y = year+1
+        current_y = snow_season[1]
         
     days_in_month = len_month(m, current_y, cfg.leapdays)
     
-    full_lat, full_lon, t2m, pr = cfg.read_month(t2m_files[i], tp_files[i], m, current_y)
-    
+    full_lat, full_lon, t2m, pr = read_month(cfg.forcing,
+                                             t2m_files[i], 
+                                             tp_files[i], 
+                                             m, current_y)
     if i == 0:
         
         latmask, lonmask = square_mask(full_lat, full_lon, 
-                                       cfg.latminmax, cfg.lonminmax, 
-                                       cfg.lon_0_360)
+                                       cfg.latminmax, cfg.lonminmax)
         
         nlats, nlons = np.sum(latmask), np.sum(lonmask)
         lats, lons = full_lat[latmask], full_lon[lonmask]
@@ -45,9 +45,12 @@ for i,m in enumerate([8,9,10,11,12,1,2,3,4,5,6,7]):
         SWEmax_record = np.zeros((nlats, nlons)) #[mm water equivalent], maximum SWE
 
         # - Set up prognostic variable grids only once - #
-        old_depth = np.zeros((nlats, nlons)) #[m snow]
+        old_depth = np.zeros((nlats, nlons)) #[m snow depth]
         old_dens = np.zeros((nlats, nlons)) #[kg/m3]
         
+    t2m_scale, tp_scale = mult_scaling(m, cfg.forcing, cfg.target_name, cfg.clim_loc,
+                                       full_lat, full_lon, latmask, lonmask, 
+                                       cfg.adjust, cfg.Unique_ID)
         
     # ----- Set up daily records for the month ----- #
     snf_record = np.zeros((nlats, nlons, days_in_month)) #[m snow], snow depth
@@ -55,22 +58,22 @@ for i,m in enumerate([8,9,10,11,12,1,2,3,4,5,6,7]):
     
     # ------------- Step through month ------------- #
     day = 0
-    for step in range(days_in_month * cfg.t2m_freq):
+    for step in range(days_in_month * t2m_freq[cfg.forcing]):
         
          # ------------ Read in precip data ------------- #
-        t2m_steps_per_pr = cfg.t2m_freq //cfg.pr_freq
-        prate = cfg.read_day(pr, 'tp', step // t2m_steps_per_pr,  latmask, lonmask)
-        prate = cfg.standardize_precip(prate) #[m water] per precipitation time step
+        t2m_steps_per_pr = t2m_freq[cfg.forcing] // tp_freq[cfg.forcing]
+        prate = read_day(cfg.forcing, pr, 'tp', step // t2m_steps_per_pr,  latmask, lonmask)
+        prate = tp_scale * standardize_precip(cfg.forcing, tp_freq[cfg.forcing], t2m_freq[cfg.forcing], prate) #[m water] per precipitation time step
         prate[prate < 0] = 0
-        ptot_record += prate 
+        ptot_record += prate  
         
         # ---------- Read in temperature data ---------- #
         if (step == 0) & (i == 0):
-            # initially use the same values for t2m last as for t2m_air
-            t2m_last = cfg.read_day(t2m, 't2m', step, latmask, lonmask) #[K]
+            # initially use the same values for t2m_last as for t2m_air
+            t2m_last = t2m_scale * read_day(cfg.forcing, t2m, 't2m', step, latmask, lonmask) #[K]
         else:
             t2m_last = t2m_air #[K]
-        t2m_air = cfg.read_day(t2m, 't2m', step, latmask, lonmask) #[K]
+        t2m_air = t2m_scale * read_day(cfg.forcing, t2m, 't2m', step, latmask, lonmask) #[K]
         TSFC = np.stack((t2m_last, t2m_air)) - 273.15 #[degrees C]  
         tavg = np.mean(TSFC, axis=0) #[degrees C]
         
@@ -78,7 +81,7 @@ for i,m in enumerate([8,9,10,11,12,1,2,3,4,5,6,7]):
         sftot_record[tavg <= 0] += prate[tavg <= 0]
         
         # --------- Linearly interpolate temp ---------- #
-        hours_per_step = 24 // cfg.t2m_freq
+        hours_per_step = 24 // t2m_freq[cfg.forcing]
         T_hr = np.ones((hours_per_step+1, TSFC.shape[1], TSFC.shape[2]))
         for hr in range(hours_per_step+1):
             T_hr[hr,:,:] = (TSFC[1,:,:] - TSFC[0,:,:]) * hr / hours_per_step + TSFC[0,:,:] #[degrees C]
@@ -93,7 +96,7 @@ for i,m in enumerate([8,9,10,11,12,1,2,3,4,5,6,7]):
         SWEmax_record = np.maximum(SWEmax_record, swe) #[mm water equivalent]
 
         # --- Record daily depth and density values ---- #
-        if (step + 1) % cfg.t2m_freq == 0: #last time step each day
+        if (step + 1) % t2m_freq[cfg.forcing] == 0: #last time step each day
             print('day ', day)
             snf_record[:,:,day] = old_depth #[m snow]
             density_record[:,:,day] = old_dens #[kg/m3]
@@ -106,11 +109,12 @@ for i,m in enumerate([8,9,10,11,12,1,2,3,4,5,6,7]):
                 
                 #set up save name according to settings
                 out_fname = cfg.output_loc + monthly_out_name(cfg.Unique_ID, 
-                                                              month_names_aug[i], 
-                                                              cfg.mixed_pr,
-                                                              year_tag)
-                save_daily(full_lat[latmask], full_lon[lonmask], times, 
-                           snf_record, density_record, out_fname)
+                                                                month_names_aug[i], 
+                                                                cfg.mixed_pr,
+                                                                year_tag)
+                save_daily(lats, lons, times, 
+                           snf_record, density_record, 
+                           out_fname)
                 
             day += 1
 
@@ -119,7 +123,9 @@ for i,m in enumerate([8,9,10,11,12,1,2,3,4,5,6,7]):
         t2m.close()      
         
 # ------ Save accumulated records to file ------ #
-save_annual(cfg.Unique_ID, cfg.output_loc, cfg.mixed_pr, year_tag, full_lat[latmask], full_lon[lonmask], ptot_record, sftot_record, SWEmax_record)
+save_annual(cfg.Unique_ID, cfg.output_loc, cfg.mixed_pr, 
+            year_tag, lats, lons, 
+            ptot_record, sftot_record, SWEmax_record)
 
 pr.close()
 t2m.close()

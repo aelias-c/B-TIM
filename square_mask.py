0868493d3aeb17db170b7b01ebb1b8d40b449dfd
aelias-c <66627234+aelias-c@users.noreply.github.com>
2023-06-22 11:31:47 -0400

    Update square_mask.py
    Removed longitude subsetting (for now).

--- square_mask.py
+++ square_mask.py
@@ -1,25 +1,11 @@
-import numpy as np
-
-def square_mask(full_lat, full_lon, latminmax, lonminmax, lon_0_360):
+def square_mask(full_lat, full_lon, latminmax, lonminmax):
     
+    # mask for latitudes
     lat_mask = (full_lat >= latminmax[0]) & (full_lat <= latminmax[1])
     
-    if lon_0_360:
-        lon_mask = (full_lon >= np.min(lonminmax)) & (full_lon <= np.max(lonminmax))
-    
-    else:
-        if lonminmax[0] < lonminmax[1]:
-            if lonminmax[0] < 180:
-                if lonminmax[1] >= 180:
-                    lon_mask = (full_lon >= lonminmax[0]) | (full_lon <= (lonminmax[1]+180)%360-180)
-                else:
-                    lon_mask = (full_lon >= lonminmax[0]) | (full_lon <= lonminmax[1])
-            else:
-                lon_mask = (full_lon >= (lonminmax[0]+180)%360-180) | (full_lon <= (lonminmax[1]+180)%360-180)                   
-        else:
-            if lonminmax[1] < 180:
-                lon_mask = (full_lon >= lonminmax[0]) | (full_lon <= lonminmax[1])
-            else:
-                lon_mask = (full_lon >= lonminmax[0]) | (full_lon <= 180) | (full_lon <= (lonminmax[1]+180)%360-180)
+    # mask for longitudes
+    lon_mask = (full_lon >= 0) | (full_lon <= 0) 
 
     return lat_mask, lon_mask
+                
+       

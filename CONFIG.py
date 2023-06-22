45c1b8fde9e5f731cdb9e419822f5018bb802daa
aelias-c <66627234+aelias-c@users.noreply.github.com>
2023-06-22 12:32:03 -0400

    Update CONFIG.py

--- CONFIG.py
+++ CONFIG.py
@@ -5,7 +5,7 @@ import sys
 year = int(sys.argv[1])
 
 forcing = str(sys.argv[2])
-data_loc = '/users/jk/20/achereque/SnowProjects2/data/01_forcing/'
+data_loc = './forcing/'
 
 mixed_pr = False
 latminmax = [10,90]
@@ -16,8 +16,8 @@ Unique_ID = forcing
 
 # - For rescaling experiments - #
 
-clim_loc = '/users/jk/20/achereque/SnowProjects2/data/01_forcing/clim/'
-
+clim_loc = './clim/'
+rescaling_weights_loc = './weights/'
 if len(sys.argv) > 3:
     adjust = str(sys.argv[3]) #tp, t2m, neither, both
     if adjust != 'neither':
@@ -29,7 +29,7 @@ else:
     
 # ----------------------------- #
 
-output_loc = '/users/jk/20/achereque/SnowProjects2/data/03_reconstruction_output/'+Unique_ID+'/'
+output_loc = './output/'+Unique_ID+'/'
 if not exists(output_loc):
     makedirs(output_loc)
     

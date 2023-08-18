d440b81ff0c7c2d1b5169a876cc9f234d001e503
aelias-c <66627234+aelias-c@users.noreply.github.com>
2023-08-18 13:50:54 -0400

    Update BTIM.py

--- BTIM.py
+++ BTIM.py
@@ -1,14 +1,14 @@
 import numpy as np
 from pandas import date_range
 
-from utils import len_month, monthly_out_name, month_names_aug, prepare_filenames, read_month, read_day, t2m_freq, tp_freq, standardize_precip
+from utils import len_month, monthly_out_name, month_names_aug, prepare_filenames, read_month, read_day, t2m_freq, tp_freq, standardize_precip, standardize_temp
 from square_mask import square_mask
 from time_step import Brasnett
 from rescaling import mult_scaling
 from save_daily import save_daily
 from save_annual import save_annual
 
-import CONFIG as cfg
+import CONFIG_Lilian as cfg
 
 # ------- Initialize -------- #
 
@@ -27,10 +27,9 @@ for i,m in enumerate([8,9,10,11,12,1,2,3,4,5,6,7]):
         
     days_in_month = len_month(m, current_y, cfg.leapdays)
     
-    full_lat, full_lon, t2m, pr = read_month(cfg.forcing,
-                                             t2m_files[i], 
-                                             tp_files[i], 
-                                             m, current_y)
+    full_lat, full_lon, t2m, pr = read_month(m, cfg.forcing, t2m_files[i], tp_files[i])
+        
+        
     if i == 0:
         
         latmask, lonmask = square_mask(full_lat, full_lon, 
@@ -39,6 +38,12 @@ for i,m in enumerate([8,9,10,11,12,1,2,3,4,5,6,7]):
         nlats, nlons = np.sum(latmask), np.sum(lonmask)
         lats, lons = full_lat[latmask], full_lon[lonmask]
         
+        if lats.size == 1:
+            lats, latmask = np.array([lats]), np.array([latmask])
+        if lons.size == 1:
+            lons, lonmask = np.array([lons]), np.array([lonmask])
+
+
         # ------ Set up records for the year once ------ #
         ptot_record = np.zeros((nlats, nlons)) #[m], total precip 
         sftot_record = np.zeros((nlats, nlons)) #[m water equivalent], total snowfall
@@ -47,11 +52,11 @@ for i,m in enumerate([8,9,10,11,12,1,2,3,4,5,6,7]):
         # - Set up prognostic variable grids only once - #
         old_depth = np.zeros((nlats, nlons)) #[m snow depth]
         old_dens = np.zeros((nlats, nlons)) #[kg/m3]
-        
+   
     t2m_scale, tp_scale = mult_scaling(m, cfg.forcing, cfg.target_name, cfg.clim_loc,
                                        full_lat, full_lon, latmask, lonmask, 
-                                       cfg.adjust, cfg.Unique_ID, cfg.rescaling_weights_loc)
-        
+                                       cfg.adjust, cfg.Unique_ID)
+    
     # ----- Set up daily records for the month ----- #
     snf_record = np.zeros((nlats, nlons, days_in_month)) #[m snow], snow depth
     density_record = np.zeros((nlats, nlons, days_in_month)) #[kg/m3], snow density
@@ -63,17 +68,22 @@ for i,m in enumerate([8,9,10,11,12,1,2,3,4,5,6,7]):
          # ------------ Read in precip data ------------- #
         t2m_steps_per_pr = t2m_freq[cfg.forcing] // tp_freq[cfg.forcing]
         prate = read_day(cfg.forcing, pr, 'tp', step // t2m_steps_per_pr,  latmask, lonmask)
-        prate = tp_scale * standardize_precip(cfg.forcing, tp_freq[cfg.forcing], t2m_freq[cfg.forcing], prate) #[m water] per precipitation time step
+        prate = tp_scale * standardize_precip(cfg.forcing, 
+                                              tp_freq[cfg.forcing], 
+                                              t2m_freq[cfg.forcing], 
+                                              prate) #[m water] per precipitation time step
         prate[prate < 0] = 0
         ptot_record += prate  
         
         # ---------- Read in temperature data ---------- #
         if (step == 0) & (i == 0):
             # initially use the same values for t2m_last as for t2m_air
-            t2m_last = t2m_scale * read_day(cfg.forcing, t2m, 't2m', step, latmask, lonmask) #[K]
+            read_t2m = read_day(cfg.forcing, t2m, 't2m', step, latmask, lonmask)
+            t2m_last = t2m_scale * standardize_temp(cfg.forcing, read_t2m) #[K]
         else:
             t2m_last = t2m_air #[K]
-        t2m_air = t2m_scale * read_day(cfg.forcing, t2m, 't2m', step, latmask, lonmask) #[K]
+        read_t2m = read_day(cfg.forcing, t2m, 't2m', step, latmask, lonmask)
+        t2m_air = t2m_scale * standardize_temp(cfg.forcing, read_t2m) #[K]
         TSFC = np.stack((t2m_last, t2m_air)) - 273.15 #[degrees C]  
         tavg = np.mean(TSFC, axis=0) #[degrees C]
         

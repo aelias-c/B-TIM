34ba447eafc85c892de0ecc6994e447e853fb7aa
aelias-c <66627234+aelias-c@users.noreply.github.com>
2023-08-18 14:03:16 -0400

    Update CONFIG.py

--- CONFIG.py
+++ CONFIG.py
@@ -1,30 +1,33 @@
-from os import makedirs
+from os import makedirs, chmod
 from os.path import exists
 import sys
 
 year = int(sys.argv[1])
 
 forcing = str(sys.argv[2])
-data_loc = './forcing/'
+data_loc = './input/'
 
 mixed_pr = False
 latminmax = [10,90]
 lonminmax = [0,360]
-leapdays = True
+leapdays = False
 
 Unique_ID = forcing
 
 # - For rescaling experiments - #
 
 clim_loc = './clim/'
-rescaling_weights_loc = './weights/'
+
 if len(sys.argv) > 3:
     adjust = str(sys.argv[3]) #tp, t2m, neither, both
+
     if adjust != 'neither':
         target_name = str(sys.argv[4])
         Unique_ID += 'r_{}_target_{}'.format(adjust, target_name)
+    else:
+        target_name = 'None'
 else:
-    target_name = 'None'
+    target_name = forcing
     adjust = 'neither'
     
 # ----------------------------- #

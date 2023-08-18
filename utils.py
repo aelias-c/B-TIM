4b73a3db14c799ac79b8c26a37891363e728c923
aelias-c <66627234+aelias-c@users.noreply.github.com>
2023-08-18 14:01:50 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,13 +5,16 @@ from xarray import open_mfdataset
 
 month_names_aug = ['Aug', 'Sept', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'March', 'April', 'May', 'June', 'July']
 
-t2m_freq = {'JRA55': 8, 'ERA5':24, 'MERRA2':24}
-tp_freq = {'JRA55': 8, 'ERA5':24, 'MERRA2':24}
-latname = {'JRA55':'latitude', 'ERA5':'latitude', 'MERRA2':'lat'}
-lonname = {'JRA55':'longitude', 'ERA5':'longitude', 'MERRA2':'lon'}
-precipname = {'JRA55':'tp', 'ERA5':'tp', 'MERRA2':'PRECTOTLAND'}
-tempname = {'JRA55':'t2m', 'ERA5':'t2m', 'MERRA2':'T2M'}
+t2m_freq = {'JRA55': 8, 'ERA5':24, 'MERRA2':24, 'CanESM5raw':1, 'CanESM5qdm':1, 'NorESM2raw':1, 'NorESM2qdm':1, 'MPIraw':1, 'MPIqdm':1, 'obs':1, 'CanESM5downscaled':1}
+tp_freq = {'JRA55': 8, 'ERA5':24, 'MERRA2':24, 'CanESM5raw':1, 'CanESM5qdm':1, 'NorESM2raw':1, 'NorESM2qdm':1, 'MPIraw':1, 'MPIqdm':1, 'obs':1, 'CanESM5downscaled':1}
 
+latname = {'JRA55':'latitude', 'ERA5':'latitude', 'MERRA2':'lat', 'CanESM5raw':'lat', 
+           'CanESM5qdm':'lat', 'NorESM2raw':'lat', 'NorESM2qdm':'lat', 'MPIraw':'lat', 'MPIqdm':'lat', 'obs':'lat', 'CanESM5downscaled':'lat'}
+lonname = {'JRA55':'longitude', 'ERA5':'longitude', 'MERRA2':'lon', 'CanESM5raw':'lon', 
+           'CanESM5qdm':'lon', 'NorESM2raw':'lon', 'NorESM2qdm':'lon', 'MPIraw':'lon', 'MPIqdm':'lon', 'obs':'lon', 'CanESM5downscaled':'lon'}
+
+precipname = {'JRA55':'tp', 'ERA5':'tp', 'MERRA2':'PRECTOTLAND', 'CanESM5raw':'pr', 'CanESM5qdm':'scen', 'NorESM2raw':'pr', 'NorESM2qdm':'scen', 'MPIraw':'pr', 'MPIqdm':'scen', 'obs':'pr', 'CanESM5downscaled':'pr'}
+tempname = {'JRA55':'t2m', 'ERA5':'t2m', 'MERRA2':'T2M', 'CanESM5raw':'tas', 'CanESM5qdm':'scen', 'NorESM2raw':'tas', 'NorESM2qdm':'scen', 'MPIraw':'tas', 'MPIqdm':'scen', 'obs':'tas', 'CanESM5downscaled':'pr'}
 
 def len_month(m, year, leapday=True):
     '''Takes in month (Jan = 1) and year, returns the number of days in the month.'''
@@ -29,23 +32,37 @@ def len_month(m, year, leapday=True):
     
 
 def prepare_filenames(forcing, data_loc, snow_season):
-    
-    prefix = lambda var,m,y: data_loc+var+'/'+forcing+'/'+forcing+'_'+var+'_'+str(m).zfill(2)+'_'+str(y)+'/'
-    
-    tp_AugDec = [[prefix('tp',i,snow_season[0])+n for n in listdir(prefix('tp',i,snow_season[0])) if '.nc' in n] for i in range(8, 13)]
-    tp_JanJul = [[prefix('tp',i,snow_season[1])+n for n in listdir(prefix('tp',i,snow_season[1])) if '.nc' in n] for i in range(1,8)]
+        
+    elif isin(forcing, ['CanESM5raw', 'CanESM5qdm', 'NorESM2raw', 'NorESM2qdm', 'MPIraw', 'MPIqdm', 'obs', 'CanESM5downscaled']):
+        pattern = lambda var,m,y: data_loc+forcing+'_'+var+'_'+str(m).zfill(2)+'_'+str(y)+'.nc'
+        
+        tp_AugDec = [pattern('tp', i, snow_season[0]) for i in range(8, 13)]
+        tp_JanJul = [pattern('tp', i, snow_season[1]) for i in range(1, 8)]
+        
+        t2m_AugDec = [pattern('t2m', i, snow_season[0]) for i in range(8, 13)]
+        t2m_JanJul = [pattern('t2m', i, snow_season[1]) for i in range(1, 8)]
+        
+    else:
+        prefix = lambda var,m,y: data_loc+var+'/'+forcing+'/'+forcing+'_'+var+'_'+str(m).zfill(2)+'_'+str(y)+'/'
 
-    t2m_AugDec = [[prefix('t2m',i,snow_season[0])+n for n in listdir(prefix('t2m',i,snow_season[0])) if '.nc' in n] for i in range(8, 13)]
-    t2m_JanJul = [[prefix('t2m',i,snow_season[1])+n for n in listdir(prefix('t2m',i,snow_season[1])) if '.nc' in n] for i in range(1,8)]
+        tp_AugDec = [[prefix('tp',i,snow_season[0]) + n for n in listdir(prefix('tp',i,snow_season[0])) if '.nc' in n] \
+                     for i in range(8, 13)]
+        tp_JanJul = [[prefix('tp',i,snow_season[1]) + n for n in listdir(prefix('tp',i,snow_season[1])) if '.nc' in n] \
+                     for i in range(1,8)]
+
+        t2m_AugDec = [[prefix('t2m',i,snow_season[0]) + n for n in listdir(prefix('t2m',i,snow_season[0])) if '.nc' in n] \
+                      for i in range(8, 13)]
+        t2m_JanJul = [[prefix('t2m',i,snow_season[1]) + n for n in listdir(prefix('t2m',i,snow_season[1])) if '.nc' in n] \
+                      for i in range(1,8)]
     
     tp_files = tp_AugDec + tp_JanJul
     t2m_files = t2m_AugDec + t2m_JanJul
     
     return t2m_files, tp_files
 
-def read_month(forcing, t2m_fname, tp_fname, month, y):
+def read_month(month, forcing, t2m_fname, tp_fname, year=None):
     
-    if forcing == 'MERRA2':
+    if isin(forcing, ['MERRA2', 'CanESM5raw', 'CanESM5qdm', 'NorESM2raw', 'NorESM2qdm', 'MPIraw', 'MPIqdm', 'obs', 'CanESM5downscaled']):
         
         t2m = open_mfdataset(t2m_fname, combine='by_coords')
         tp = open_mfdataset(tp_fname, combine='by_coords')
@@ -69,12 +86,29 @@ def read_month(forcing, t2m_fname, tp_fname, month, y):
 
 def read_day(forcing, data, forcing_var, step, latmask, lonmask):
     
+    
     decode_var = {'tp':precipname[forcing], 't2m':tempname[forcing]}
     
-    if forcing == 'MERRA2':
-        output = data[decode_var[forcing_var]][step,latmask,lonmask].values
+    if isin(forcing, ['MERRA2', 'CanESM5raw', 'CanESM5qdm', 'NorESM2raw', 'NorESM2qdm', 'MPIraw', 'MPIqdm', 'obs', 'CanESM5downscaled']):
+        
+        if (latmask.size == 1) & (lonmask.size == 1):
+            output = data[decode_var[forcing_var]][step].values
+        elif (latmask.size == 1):
+            output = data[decode_var[forcing_var]][step,lonmask].values
+        elif (lonmask.size == 1):
+            output = data[decode_var[forcing_var]][step,latmask].values
+        else:
+            output = data[decode_var[forcing_var]][step,latmask,lonmask].values
+            
     else:
-        output = data[decode_var[forcing_var]][step,latmask,lonmask]
+        if (latmask.size == 1) & (lonmask.size == 1):
+            output = data[decode_var[forcing_var]][step]
+        elif (latmask.size == 1):
+            output = data[decode_var[forcing_var]][step,lonmask]
+        elif (lonmask.size == 1):
+            output = data[decode_var[forcing_var]][step,latmask]
+        else:
+            output = data[decode_var[forcing_var]][step,latmask,lonmask]
 
     return output
 
@@ -84,19 +118,33 @@ def standardize_precip(forcing, pr_freq, t2m_freq, data):
     if forcing == 'JRA55':
         data = data / (pr_freq * 1000) #[m/tp step from mm/day]
         data = (pr_freq/t2m_freq) * data #[m/t2m step] 
+        
     elif forcing == 'MERRA2':
         data = 3600 * data / 1000 #[m/precip step] from [mm/s]
         data = (pr_freq/t2m_freq) * data #[m/t2m step]
+        
+    elif isin(forcing, ['CanESM5raw', 'CanESM5qdm', 'NorESM2raw', 'NorESM2qdm', 'MPIraw', 'MPIqdm', 'obs', 'CanESM5downscaled']):
+        data = data / 1000 #[m/precip step] from [kg/m2/day]
+        data = (pr_freq/t2m_freq) * data #[m/t2m step]
+        
     elif forcing == 'ERA5':
         data = (pr_freq/t2m_freq) * data #[m/t2m step]
+   
+    return data  
+
+def standardize_temp(forcing, data):
+    '''Convert into [K] from native units.'''
+    
+    if isin(forcing, ['CanESM5raw', 'CanESM5qdm', 'NorESM2raw', 'NorESM2qdm', 'MPIraw', 'MPIqdm', 'obs', 'CanESM5downscaled']):
+        data = data + 273.15 # [K] from [degC]
    
     return data  
     
 def monthly_out_name(Unique_ID, month, mixed_pr, year_tag):
     
     savename = Unique_ID + '_forced_swe_' + month + '_'
-
-    if mixed_pr:
+   
+    if mixed_pr[0] != mixed_pr[1]:
         savename += 'mixedpr_'
         
     savename = savename + year_tag + '.nc'
